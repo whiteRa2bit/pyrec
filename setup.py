ad192b61c19485b35d6d43cd537e6f59cac3525c
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-06-27 15:46:59 +0300

    Add yapf + update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", 'r') as f:
 requirements = []
 
 setup(
-   name='pyrec',
-   version='0.1',
-   description='Python library for recommender systems algorithms',
-   license="MIT",
-   long_description=long_description,
-   author='Pavel Fakanov',
-   author_email='pavel.fakanov@gmail.com',
-   packages=find_packages(),
-   install_requires=requirements,
+    name='pyrec',
+    version='0.1',
+    description='Python library for recommender systems algorithms',
+    license="MIT",
+    long_description=long_description,
+    author='Pavel Fakanov',
+    author_email='pavel.fakanov@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements,
 )
