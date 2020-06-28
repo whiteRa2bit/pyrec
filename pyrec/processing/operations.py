8629e84a5bc6652c2e82ee990b4ed67f73ce68b4
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-06-28 15:11:45 +0300

    Fix ypf for saving

--- pyrec/processing/operations.py
+++ pyrec/processing/operations.py
@@ -12,3 +12,25 @@ class Operation(ABC):
     @abstractmethod
     def __call__(self, rows: TRowsIterable, *args: tp.Any, **kwargs: tp.Any) -> TRowsGenerator:
         pass
+
+# Operations
+
+
+class Mapper(ABC):
+    """Base class for mappers"""
+    @abstractmethod
+    def __call__(self, row: TRow) -> TRowsGenerator:
+        """
+        :param row: one table row
+        """
+        pass
+
+
+class Map(Operation):
+    def __init__(self, mapper: Mapper) -> None:
+        self.mapper = mapper
+
+    def __call__(self, rows: TRowsIterable, *args: tp.Any, **kwargs: tp.Any) -> TRowsGenerator:
+        for row in rows:
+            yield from self.mapper(row)
+
