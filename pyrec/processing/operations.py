ee3940f7c2250a161d9aca8f2e6b721f43856a5f
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-06-28 15:13:40 +0300

    Add reduce operation

--- pyrec/processing/operations.py
+++ pyrec/processing/operations.py
@@ -35,3 +35,28 @@ class Map(Operation):
     def __call__(self, rows: TRowsIterable, *args: tp.Any, **kwargs: tp.Any) -> TRowsGenerator:
         for row in rows:
             yield from self.mapper(row)
+
+
+class Reducer(ABC):
+    """Base class for reducers"""
+
+    @abstractmethod
+    def __call__(self, group_key: tp.Tuple[str, ...], rows: TRowsIterable) -> TRowsGenerator:
+        """
+        :param rows: table rows
+        """
+
+        pass
+
+
+class Reduce(Operation):
+    def __init__(self, reducer: Reducer, keys: tp.Sequence[str]) -> None:
+        self.reducer = reducer
+        self.keys = keys
+
+    def __call__(self, rows: TRowsIterable, *args: tp.Any, **kwargs: tp.Any) -> TRowsGenerator:
+        if self.keys:
+            for group_keys, group_rows in groupby(rows, key=itemgetter(*self.keys)):
+                yield from self.reducer(tuple(self.keys), group_rows)
+        else:
+            yield from self.reducer(tuple(self.keys), rows)
