210f963639ca726ac3906974cc9c21b63acc9274
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-06-28 15:08:21 +0300

    Add Operation class

---
+++ pyrec/processing/operations.py
@@ -0,0 +1,14 @@
+from abc import abstractmethod, ABC
+import typing as tp
+from itertools import groupby
+from operator import itemgetter
+
+TRow = tp.Dict[str, tp.Any]
+TRowsIterable = tp.Iterable[TRow]
+TRowsGenerator = tp.Generator[TRow, None, None]
+
+
+class Operation(ABC):
+    @abstractmethod
+    def __call__(self, rows: TRowsIterable, *args: tp.Any, **kwargs: tp.Any) -> TRowsGenerator:
+        pass
